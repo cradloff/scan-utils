b6db9854ff1bd03699cdb4abf8d2f57892ce7da8
Claus Radloff <Claus.Radloff@web.de>
2018-06-28 23:48:39 +0200

    Shell Scripte zum Starten der Programme erstellt

---
+++ textpostprocess.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+# Path where script resides
+SU_PATH=`dirname $0`
+# Path to jar archive
+JAR=`ls $SU_PATH/scan-utils-*-jar-with-dependencies.jar`
+
+java -classpath $SU_PATH:$JAR org.github.cradloff.scanutils.PostProcess "$@"
+
