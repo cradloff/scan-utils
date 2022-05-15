7a73af0d70201b4bb7bdee2688cbc7ae94d16d33
Claus Radloff <claus.radloff@web.de>
2022-05-15 16:55:35 +0200

    Ersetzung von Kapitel-Überschriften verbessert

--- src/main/java/org/github/cradloff/scanutils/PrepareText.java
+++ src/main/java/org/github/cradloff/scanutils/PrepareText.java
@@ -248,7 +248,7 @@ public class PrepareText {
 	}
 
 	private static String handleChapter(String line) {
-		return line.replaceAll("^(\\d)[.,:]? [AFKNR]a[pv][it][ti]e[lt]\\s*.*$", "<h2>$1. Kapitel.</h2>");
+		return line.replaceAll("^[^\\p{Alnum}]*(\\d)[.,:]? [AFKNR]a[pv][it][ti]e[lt]\\s*.*$", "<h2>$1. Kapitel.</h2>");
 	}
 
 	private static String handleSubChapter(String previousLine, String line) {
